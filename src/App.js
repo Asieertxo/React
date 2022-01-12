24c694f0c036e1d4d6692468f076e3d814bbfcfd
Asieertxo <asgal.gl@gmail.com>
2022-01-12 13:52:30 +0100

    Expresion con objeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
 function App() {
-  const h = "Hola Mundo";
+  const title = "React";
   return (
     <div className="App">
-      <h1>{h}</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"></input>
     </div>
   );
 }
